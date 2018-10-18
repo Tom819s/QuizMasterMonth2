00df1edbdf64e0e810a632331bd38017a562cf8e
Thomas Martin <tjmartin2@student.fullsail.edu>
2018-10-18 15:14:15 -0400

    loginhandler b now works

--- app/src/main/java/pp2/fullsailuniversity/secondbuild/MainActivity.java
+++ app/src/main/java/pp2/fullsailuniversity/secondbuild/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 
 import com.google.android.gms.auth.api.Auth;
@@ -186,6 +187,13 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
 
 
+    }
+
+    public void loginHandlerB(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
 
 
